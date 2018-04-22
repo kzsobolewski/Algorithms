f6b457fbdb96ebd7eab5bde6f507df6c9ca2a7b7
kzsobolewski <kzsobolewski@gmail.com>
2018-04-22 02:16:43 +0200

    first commit of marge Sort, doesn't work LOL

--- quickSort.cpp
+++ quickSort.cpp
@@ -8,6 +8,58 @@ using namespace std;
  * dla 100 tablic o losowych wartosciach
  *
  */
+
+template<typename T>
+void merge(T *tab, int from, int to, int mid){
+  int i = from;
+  int j = mid + 1;
+  int k = 0;
+
+  T tmpArray[to - from  + 1];
+
+  // scala 2. czesci w jedna tablice
+  while(i <= mid && j<= to){
+    if(tab[i] < tab[j]){
+      tmpArray[k] = tab[j];
+      k++; i++;
+    } else {
+      tmpArray[k] = tab[j];
+      k++; j++;
+    }
+  }
+
+  // wstawia pozostale wartosci od i do mid w tablice tmpArray
+  while(i<=mid){
+    tmpArray[k] = tab[i];
+    k++; i++;
+  }
+
+  // wstawia pozostale wartosci of j do to w tablice tmpArray
+  while(j <= to){
+    tmpArray[k] = tab[j];
+    k++; j++;
+  }
+
+  // przepisuje posortowane dane do oryginalnej tabicy
+  for( i = from; i<= to ; i++)
+    tab[i] = tmpArray[i - from];
+
+}
+
+template<typename T>
+void mergeSort(T *tab, int from, int to){
+  int mid;
+  if(from < to){
+      mid = (from+to)/2;
+
+      mergeSort(tab, from, mid);
+      mergeSort(tab, mid+1 , to);
+
+      merge(tab, from, to, mid);
+  }
+}
+
+
 template<typename T>
 void quickSort( T *tab, int left, int right )
 {
@@ -39,9 +91,9 @@ void reverse(T *tab, int left, int right){
 }
 
 template<typename T>
-void print(T *arr, int size){
+void print(T *tab, int size){
   for(int i =0; i<size; i++)
-    cout << arr[i] << "  ";
+    cout << tab[i] << "  ";
   cout << endl;
 }
 
@@ -49,7 +101,8 @@ template<typename T>
 void randomize(T **arr,int  size, int qnt){
   for(int j=0; j<qnt;j++){
     for(int i = 0 ; i < size ; i++)
-      arr[j][i] = rand()%1000001-500000;
+    //  arr[j][i] = rand()%1000001-500000;
+      arr[j][i] = rand()%100;
   }
 }
 
@@ -74,7 +127,7 @@ void sortPart(T **arr, int size, int qnt, double perc){
 int main()
 {
   srand( time( NULL ) );
-  int size = 100000;
+  int size = 10;
   int quantity = 100;
 
   cout << "\nSorting 100 arrays with " << size << " elements\n";
@@ -93,8 +146,12 @@ int main()
   clock_t start, stop; //inicjacja zmiennych zegarowych
   start=clock(); //zapisanie czasu startu mierzenia
 
-  for(int i = 0; i<quantity; i++)
-    quickSort(array[i], 0, size-1);
+    //quickSort(array[i], 0, size-1);
+    print(array[0], size);
+    for(int i = 0; i<quantity; i++)
+    mergeSort(array[i], 0, size-1);
+    cout << endl << endl;
+    print(array[0], size);
 
   stop=clock();//zapisanie konca mierzenia
   cout << "sorted!\n";
